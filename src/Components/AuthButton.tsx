034f2707aa73945adb869567d078024bc4fda8bb
Nelum2000731 <pushpik-ps20289@stu.kln.ac.lk>
2025-01-05 09:03:04 +0530

    Change AuthButton.tsx file

--- src/Components/AuthButton.tsx
+++ src/Components/AuthButton.tsx
@@ -2,16 +2,24 @@ import React from 'react';
 import './AuthButton.css';
 
 interface AuthButtonProps {
-  label: string; 
-  onClick: () => void; 
-  disabled?: boolean; 
+  label: string;
+  onClick: () => void;
+  disabled?: boolean;
 }
 
-const AuthButton: React.FC<AuthButtonProps> = ({ label, onClick, disabled = false }) => {
+const AuthButton: React.FC<AuthButtonProps> = ({
+  label,
+  onClick,
+  disabled = false,
+}) => {
   return (
-    <button className={'auth-button ${disabled ? 'disabled' : ''}'} onClick={onClick} disabled={disabled}>
-        {label}
-        </button>
+    <button
+      className={`auth-button ${disabled ? 'disabled' : ''}`}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {label}
+    </button>
   );
 };
 
