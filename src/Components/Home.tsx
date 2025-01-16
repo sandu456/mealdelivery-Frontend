d0b0462e7af1d9f7a8425cae7746f70b8ecd61d8
Asangiime <sandeep-ps20292@stu.kln.ac.lk>
2025-01-16 13:56:10 +0530

    remove

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -2,19 +2,5 @@ import React from 'react';
 
 const Home: React.FC = () => {
   return (
-    <div className="home">
-      <h1>Welcome to Meal Delivery</h1>
-      <p>Explore our meal options below:</p>
-
-      <div className="meal-categories">
-        <div className="category">
-          <h2>Vegan</h2>
-          <p>Discover our delicious vegan recipes.</p>
-          <a href="/vegan" className="category-link">Explore Vegan</a>
-        </div>
-        <div className="category">
-          <h2>Vegetarian</h2>
-          <p>Taste our wide variety of vegetarian meals.</p>
-          <a href="/vegetarian" className="category-link">Explore Vegetarian</a>
-        </div>
+    
         
