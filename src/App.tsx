baa6a4e70af90afc8b8f241f8f2046f32b6a440c
Nelum2000731 <pushpik-ps20289@stu.kln.ac.lk>
2025-01-24 20:49:35 +0530

    admin

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import AdminPage from './Pages/AdminPage';
+import CustomerPage from './Pages/CustomerPage';
 import LogIn from './Pages/LogIn';
 import SignUp from './Pages/SignUp';
 import AboutUs from './Pages/AboutUs';
@@ -10,24 +11,24 @@ import PurchaseRecipe from './Components/PurchaseRecipe';
 import Vegan from './Pages/vegan';
 import Vegetarian from './Pages/vegetarian';
 import Pescatarian from './Pages/pescatarian';
-import OrderButtons from './Components/OrderButtons';
 
 const App: React.FC = () => {
   return (
     <Router>
       <nav className="navbar">
-      <h1>Meal Delivery </h1>
-     
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><a href="/About-Us">About Us</a></li>
-        <li><a href="/Contact">Contact</a></li>
-      </ul>
-    </nav>
-    
+        <h1>Meal Delivery</h1>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/admin">Admin</Link></li>
+          <li><Link to="/customer">Customer</Link></li>
+          <li><Link to="/about-us">About Us</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+      </nav>
+
       <div className="app">
         <Routes>
-         
+          {/* Home Route */}
           <Route
             path="/"
             element={
@@ -35,63 +36,43 @@ const App: React.FC = () => {
                 <h1>Welcome to Meal Delivery</h1>
                 <p>Explore our meal options below:</p>
                 <div className="login-signup">
-                  <Link to="/login" className="button login-button">
-                    Log In
-                  </Link>
-                  <Link to="/signup" className="button signup-button">
-                    Sign Up
-                  </Link>
+                  <Link to="/login" className="button login-button">Log In</Link>
+                  <Link to="/signup" className="button signup-button">Sign Up</Link>
                 </div>
                 <div className="meal-categories">
                   <div className="category">
                     <h2>Vegan</h2>
                     <p>Discover our delicious vegan recipes.</p>
-                    <a href="/vegan" className="category-link">
-                      Explore Vegan
-                    </a>
+                    <Link to="/vegan" className="category-link">Explore Vegan</Link>
                   </div>
                   <div className="category">
                     <h2>Vegetarian</h2>
                     <p>Taste our wide variety of vegetarian meals.</p>
-                    <a href="/vegetarian" className="category-link">
-                      Explore Vegetarian
-                    </a>
+                    <Link to="/vegetarian" className="category-link">Explore Vegetarian</Link>
                   </div>
                   <div className="category">
                     <h2>Pescatarian</h2>
                     <p>Enjoy fresh pescatarian dishes.</p>
-                    <a href="/pescatarian" className="category-link">
-                      Explore Pescatarian
-                    </a>
+                    <Link to="/pescatarian" className="category-link">Explore Pescatarian</Link>
                   </div>
                 </div>
-                {/* <div className="order-options">
-                  <div className="option">
-                    <h2>Order Meals</h2>
-                    <p>Choose your favorite meals and have them delivered.</p>
-                    <Link to="/order-meal" className="option-link">
-                      Order Meals
-                    </Link>
-                  </div>
-                  <div className="option">
-                    <h2>Purchase Recipes</h2>
-                    <p>Get our exclusive recipes delivered to your email.</p>
-                    <Link to="/purchase-recipe" className="option-link">
-                      Purchase Recipes
-                    </Link>
-                  </div>
-                </div> */}
               </div>
             }
           />
 
-          
+          {/* Admin and Customer Routes */}
+          <Route path="/admin" element={<AdminPage />} />
+          <Route path="/customer" element={<CustomerPage />} />
+
+          {/* Authentication Routes */}
           <Route path="/login" element={<LogIn />} />
           <Route path="/signup" element={<SignUp />} />
 
-          <Route path="/" element={<OrderButtons />} />
+          {/* Informational Routes */}
           <Route path="/about-us" element={<AboutUs />} />
           <Route path="/contact" element={<Contact />} />
+
+          {/* Meal and Recipe Routes */}
           <Route path="/order-meal" element={<OrderMeal />} />
           <Route path="/purchase-recipe" element={<PurchaseRecipe />} />
           <Route path="/vegan" element={<Vegan />} />
@@ -99,13 +80,11 @@ const App: React.FC = () => {
           <Route path="/pescatarian" element={<Pescatarian />} />
         </Routes>
       </div>
+
       <footer className="footer">
         <p>&copy; 2025 Meal Plans and Delivery. All rights reserved.</p>
       </footer>
-      
     </Router>
-   
-    
   );
 };
 
