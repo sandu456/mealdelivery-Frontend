a51aa0ae7ecd6d3d8a696e58eacfbbf2b36a4562
Asangiime <sandeep-ps20292@stu.kln.ac.lk>
2025-03-10 13:18:56 +0530

    order buttn

--- src/Components/OrderButtons.tsx
+++ src/Components/OrderButtons.tsx
@@ -5,26 +5,26 @@ import './OrderButtons.css';
 interface OrderButtonsProps {
   onOrder: () => void;
   onPurchase: () => void;
-  onReview: () => void; // Review prop should be typed
+  onReview: () => void; 
 }
 
 const OrderButtons: React.FC<OrderButtonsProps> = ({ onOrder, onPurchase, onReview }) => {
   const navigate = useNavigate();
 
   const handleOrderNow = () => {
-    onOrder(); // Trigger the passed-in onOrder function
-    navigate('/order-meal'); // Navigates to the OrderMeal page
+    onOrder(); 
+    navigate('/order-meal'); 
   };
 
   const handlePurchaseRecipe = () => {
-    onPurchase(); // Trigger the passed-in onPurchase function
-    navigate('/purchase-recipe'); // Navigates to the OrderMeal page
+    onPurchase(); 
+    navigate('/purchase-recipe');
     alert('Feature to purchase recipes coming soon!');
   };
 
   const handleReview = () => {
-    onReview(); // Trigger the passed-in onReview function
-    navigate('/reviews'); // Navigate to the review page (adjust the route accordingly)
+    onReview(); 
+    navigate('/reviews'); 
   };
 
   return (
